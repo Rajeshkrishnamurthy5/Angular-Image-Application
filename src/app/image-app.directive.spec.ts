587d185b1226dbd7a3ed372547a3415411cf0579
Rajeshkrishnamurthy5 <rajesh.krishnamurthy@oneadvanced.com>
2020-03-22 00:02:37 +0530

    Uploaded the Application

---
+++ src/app/image-app.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImageAppDirective } from './image-app.directive';
+
+describe('ImageAppDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImageAppDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyADziPoSYM3CZ5zLAJUyYGF1pQ5xH21oAU",
+    authDomain: "angular-image-application.firebaseapp.com",
+    databaseURL: "https://angular-image-application.firebaseio.com",
+    projectId: "angular-image-application",
+    storageBucket: "angular-image-application.appspot.com",
+    messagingSenderId: "511784053611",
+    appId: "1:511784053611:web:23e573a83c57b6b63efa2e",
+    measurementId: "G-FRQNNR2CQD"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
